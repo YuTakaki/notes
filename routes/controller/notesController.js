80fddddf199c21b7af9efa465b5d056e1935dec9
yutakaki <yu.takaki2000@gmail.com>
2021-10-25 11:40:55 +0900

    notes controller finish

--- routes/controller/notesController.js
+++ routes/controller/notesController.js
@@ -7,35 +7,52 @@ const addNote = async (req, res) => {
 			.insert({ notes })
 			.returning("*")
 			.into("notes");
-		res.send(addedNote);
+		res.send(addedNote[0]);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const deleteNote = (req, res) => {
+const deleteNote = async (req, res) => {
 	try {
+		const { id } = req.params;
+		await db.delete().from("notes").where({ id });
+		res.send(id);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const updateNote = (req, res) => {
+const updateNote = async (req, res) => {
 	try {
+		const { id } = req.params;
+		const { notes } = req.body;
+		const note = await db("notes")
+			.update({ notes })
+			.where({ id })
+			.returning("*");
+		res.send(note[0]);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const allNotes = (req, res) => {
+const allNotes = async (req, res) => {
 	try {
+		const notes = await db("notes").select("*");
+		res.send(notes);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const getNote = (req, res) => {
+const getNote = async (req, res) => {
 	try {
+		const { id } = req.params;
+		const note = await db("notes")
+			.select("*")
+			.where({ id });
+		res.send(note[0]);
 	} catch (error) {
 		console.log(error);
 	}
