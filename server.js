f1ad05bfa507ffc6fe61893ddba54e4743b79563
yutakaki <yu.takaki2000@gmail.com>
2021-10-25 10:00:23 +0900

    check if production in server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("./client/build"));
+}
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
